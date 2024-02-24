7af0056dcca77c0a1665c0b700209664d62e1699
Stefan <choephix@gmail.com>
2024-02-24 21:53:41 +0000

    Update font size and add random animal emojis

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,142 @@ import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
+function getRandomEmoji(): string {
+  const emojis = [
+    '😀',
+    '😃',
+    '😄',
+    '😁',
+    '😆',
+    '😅',
+    '😂',
+    '🤣',
+    '😊',
+    '😇',
+    '🙂',
+    '🙃',
+    '😉',
+    '😌',
+    '😍',
+    '🥰',
+    '😘',
+    '😗',
+    '😙',
+    '😚',
+    '😋',
+    '😛',
+    '😝',
+    '😜',
+    '🤪',
+    '🤨',
+    '🧐',
+    '🤓',
+    '😎',
+    '⭐',
+    '🤩',
+    '🥳',
+    '😏',
+    '😒',
+    '😞',
+    '😔',
+    '😟',
+    '😕',
+    '🙁',
+    '☹️',
+    '😣',
+    '😖',
+    '😫',
+    '😩',
+    '🥺',
+    '😢',
+    '😭',
+    '😤',
+    '😠',
+    '😡',
+  ];
+
+  const randomIndex = Math.floor(Math.random() * emojis.length);
+  return emojis[randomIndex];
+}
+
+function getRandomAnimalEmoji(): string {
+  const animalEmojis = [
+    '🐶',
+    '🐱',
+    '🐭',
+    '🐹',
+    '🐰',
+    '🦊',
+    '🐻',
+    '🐼',
+    '🐻‍❄️',
+    '🐨',
+    '🐯',
+    '🦁',
+    '🐮',
+    '🐷',
+    '🐽',
+    '🐸',
+    '🐵',
+    '🙈',
+    '🙉',
+    '🙊',
+    '🐒',
+    '🐔',
+    '🐧',
+    '🐦',
+    '🐤',
+    '🐣',
+    '🐥',
+    '🦆',
+    '🦅',
+    '🦉',
+    '🦇',
+    '🐺',
+    '🐗',
+    '🐴',
+    '🦄',
+    '🐝',
+    '🐬',
+    '🐛',
+    '🦋',
+    '🐌',
+    '🐞',
+    '🐜',
+    '🧙‍♂️',
+    '🖐',
+    '🐓',
+    '🦟',
+    '🦗',
+    '🕷️',
+    '🕸️',
+    '🦂',
+  ];
+
+  const randomIndex = Math.floor(Math.random() * animalEmojis.length);
+  return animalEmojis[randomIndex];
+}
+
+const shapes = [
+  <circle cx='50' cy='50' r='40' fill='LimeGreen' />,
+  <polygon points='50,5 95,25 95,75 50,95 5,75 5,25' fill='RoyalBlue' />,
+  <polygon points='50,5 100,100 5,100' fill='Crimson' />,
+];
+
 function App() {
   const [letter, setLetter] = useState('B');
-  const [shape, setShape] = useState<JSX.Element | null>(null);
 
   const handleKeyDown: (ev: KeyboardEvent) => any = event => {
     const key = event.key;
     if (/^[a-zA-Z0-9]$/.test(key)) {
       const letter = key.toUpperCase();
       setLetter(letter);
+    } else if (key === ' ') {
+      const symbol = ' ';
+      setLetter(symbol);
     } else {
-      setLetter('-');
+      const symbol = getRandomAnimalEmoji();
+      setLetter(symbol);
     }
   };
 
@@ -24,15 +149,11 @@ function App() {
     };
   }, []);
 
-  useEffect(() => {
-    const shapes = [
-      <circle cx='50' cy='50' r='40' fill='LimeGreen' />,
-      <polygon points='50,5 95,25 95,75 50,95 5,75 5,25' fill='RoyalBlue' />,
-      <polygon points='50,5 100,100 5,100' fill='Crimson' />,
-    ];
-    const randomShape = shapes[Math.floor(Math.random() * shapes.length)];
-    setShape(randomShape);
-  }, [letter]);
+  const shape = shapes[Math.floor(Math.random() * shapes.length)];
+
+  const getRandomPercentage = () => {
+    return `${Math.random() * 20 + 40}%`;
+  };
 
   return (
     <div className='letter-container'>
@@ -42,7 +163,16 @@ function App() {
         </svg>
       </div>
 
-      <div className='center letter'>{letter}</div>
+      <div
+        className='center letter'
+        style={{
+          position: 'absolute',
+          top: getRandomPercentage(),
+          left: getRandomPercentage(),
+        }}
+      >
+        {letter}
+      </div>
     </div>
   );
 }
