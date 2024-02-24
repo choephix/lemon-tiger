70a4b58f6702a3406c29d446c6a4d68773a6b858
Stefan <choephix@gmail.com>
2024-02-24 21:22:36 +0000

    Add center alignment and shape to letter container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 function App() {
   const [letter, setLetter] = useState('B');
+  const [shape, setShape] = useState<JSX.Element | null>(null);
 
   const handleKeyDown: (ev: KeyboardEvent) => any = event => {
     const key = event.key;
@@ -23,9 +24,25 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    const shapes = [
+      <circle cx='50' cy='50' r='40' fill='LimeGreen' />,
+      <polygon points='50,5 95,25 95,75 50,95 5,75 5,25' fill='RoyalBlue' />,
+      <polygon points='50,5 100,100 5,100' fill='Crimson' />,
+    ];
+    const randomShape = shapes[Math.floor(Math.random() * shapes.length)];
+    setShape(randomShape);
+  }, [letter]);
+
   return (
     <div className='letter-container'>
-      <div className='letter'>{letter}</div>
+      <div className='center shape'>
+        <svg height='60vh' width='60vw' viewBox='0 0 100 100'>
+          {shape}
+        </svg>
+      </div>
+
+      <div className='center letter'>{letter}</div>
     </div>
   );
 }
