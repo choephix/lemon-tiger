dcf38abc3c473e42d89af0fb7dbc98d298200693
Stefan Ginev <choephix@gmail.com>
2024-02-26 10:22:38 +0200

    Update .gitignore and package.json

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+const isDev = process.env.NODE_ENV !== 'production';
+
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
-    }
-  })
+    },
+  });
 
-  win.loadURL('http://localhost:3000')
+  win.loadURL(
+    isDev ? 'http://localhost:5273' : `file://${path.join(__dirname, '../dist/index.html')}`
+  );
+  // win.loadURL('http://localhost:3000');
+
+  // Hide the menu bar
+  win.setMenuBarVisibility(false);
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
