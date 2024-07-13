437372f5e542e9110f0ac0fe7b98d831741de9cb
stefan <stefan@revoltage.io>
2024-07-13 07:24:09 +0000

    deterministic emojis

--- src/App.tsx
+++ src/App.tsx
@@ -1,128 +1,71 @@
-import React, { useState, useEffect } from 'react';
-import { debounce, throttle } from 'lodash';
+import React, { useState, useEffect } from "react";
+import { debounce, throttle } from "lodash";
 
-import './App.css';
+import "./App.css";
+import { keyToEmoji } from "./keyToEmoji";
 
-function getRandomEmoji(): string {
-  const emojis = [
-    '😀',
-    '😃',
-    '😄',
-    '😁',
-    '😆',
-    '😅',
-    '😂',
-    '🤣',
-    '😊',
-    '😇',
-    '🙂',
-    '🙃',
-    '😉',
-    '😌',
-    '😍',
-    '🥰',
-    '😘',
-    '😗',
-    '😙',
-    '😚',
-    '😋',
-    '😛',
-    '😝',
-    '😜',
-    '🤪',
-    '🤨',
-    '🧐',
-    '🤓',
-    '😎',
-    '⭐',
-    '🤩',
-    '🥳',
-    '😏',
-    '😒',
-    '😞',
-    '😔',
-    '😟',
-    '😕',
-    '🙁',
-    '☹️',
-    '😣',
-    '😖',
-    '😫',
-    '😩',
-    '🥺',
-    '😢',
-    '😭',
-    '😤',
-    '😠',
-    '😡',
-  ];
-
-  const randomIndex = Math.floor(Math.random() * emojis.length);
-  return emojis[randomIndex];
-}
-
-function getRandomAnimalEmoji(): string {
+function getRandomAnimalEmoji(keyCode: number): string {
   const animalEmojis = [
-    '🐶',
-    '🐱',
-    '🐭',
-    '🐹',
-    '🐰',
-    '🦊',
-    '🐻',
-    '🐼',
-    '🐻‍❄️',
-    '🐨',
-    '🐯',
-    '🦁',
-    '🐮',
-    '🐷',
-    '🐽',
-    '🐸',
-    '🐵',
-    '🙈',
-    '🙉',
-    '🙊',
-    '🐒',
-    '🐔',
-    '🐧',
-    '🐦',
-    '🐤',
-    '🐣',
-    '🐥',
-    '🦆',
-    '🦅',
-    '🦉',
-    '🦇',
-    '🐺',
-    '🐗',
-    '🐴',
-    '🦄',
-    '🐝',
-    '🐬',
-    '🐛',
-    '🦋',
-    '🐌',
-    '🐞',
-    '🐜',
-    '🧙‍♂️',
-    '🖐',
-    '🐓',
-    '🦟',
-    '🦗',
-    '🕷️',
-    '🕸️',
-    '🦂',
+    "🐵",
+    "🙈",
+    "🙉",
+    "🙊",
+    "🐒",
+    "🐔",
+    "🐧",
+    "🐦",
+    "🐤",
+    "🐣",
+    "🐥",
+    "🦆",
+    "🦅",
+    "🦉",
+    "🦇",
+    "🐺",
+    "🐗",
+    "🐴",
+    "🦄",
+    "🐝",
+    "🐬",
+    "🐛",
+    "🦋",
+    "🐌",
+    "🐞",
+    "🐜",
+    "🧙‍♂️",
+    "🖐",
+    "🐓",
+    "🦟",
+    "🦗",
+    "🕷️",
+    "🕸️",
+    "🦂",
+    "🐹",
+    "🦊",
+    "🐭",
+    "🐱",
+    "🐷",
+    "🐶",
+    "🐻",
+    "🐼",
+    "🐻",
+    "🐨",
+    "🐯",
+    "🐰",
+    "🦁",
+    "🐮",
+    "🐸",
   ];
 
-  const randomIndex = Math.floor(Math.random() * animalEmojis.length);
-  return animalEmojis[randomIndex];
+  // const index = Math.floor(Math.random() * animalEmojis.length);
+  const index = keyCode % animalEmojis.length;
+  return animalEmojis[index];
 }
 
 const shapes = [
-  <circle cx='50' cy='50' r='40' fill='LimeGreen' />,
-  <polygon points='50,5 95,25 95,75 50,95 5,75 5,25' fill='RoyalBlue' />,
-  <polygon points='50,5 100,100 5,100' fill='Crimson' />,
+  <circle cx="50" cy="50" r="40" fill="LimeGreen" />,
+  <polygon points="50,5 95,25 95,75 50,95 5,75 5,25" fill="RoyalBlue" />,
+  <polygon points="50,5 100,100 5,100" fill="Crimson" />,
 ];
 
 let lastKeypressTime = 0;
@@ -132,27 +75,30 @@ const THRESHOLD_MS = 250;
 
 let i = 0;
 function App() {
-  const [letter, setLetter] = useState('B');
+  const [letter, setLetter] = useState("B");
 
   const updateLetterFromKeyboardKey = (event: KeyboardEvent) => {
     const key = event.key;
     if (/^[a-zA-Z0-9]$/.test(key)) {
       const letter = key.toUpperCase();
       setLetter(letter);
-    } else if (key === ' ') {
-      const symbol = ' ';
+    } else if (key === " ") {
+      const symbol = " ";
       setLetter(symbol);
     } else {
-      const symbol = getRandomAnimalEmoji();
+      const symbol = keyToEmoji[key] ?? getRandomAnimalEmoji(event.keyCode);
       setLetter(symbol);
     }
 
     debouncing = false;
   };
 
-  const updateLetterFromKeyboardKeyDebounced = debounce(updateLetterFromKeyboardKey, 1200);
+  const updateLetterFromKeyboardKeyDebounced = debounce(
+    updateLetterFromKeyboardKey,
+    1200
+  );
 
-  const handleKeyDown: (ev: KeyboardEvent) => any = event => {
+  const handleKeyDown: (ev: KeyboardEvent) => any = (event) => {
     event.preventDefault();
 
     if (!document.fullscreenElement) {
@@ -167,7 +113,7 @@ function App() {
       }
       updateLetterFromKeyboardKey(event);
     } else {
-      setLetter('');
+      setLetter("");
       updateLetterFromKeyboardKeyDebounced(event);
       debouncing = true;
     }
@@ -179,16 +125,18 @@ function App() {
   };
 
   useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    window.addEventListener('contextmenu', disableContextMenu);
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("contextmenu", disableContextMenu);
 
     return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-      window.removeEventListener('contextmenu', disableContextMenu);
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("contextmenu", disableContextMenu);
     };
   }, []);
 
-  const shape = letter ? shapes[Math.floor(Math.random() * shapes.length)] : null;
+  const shape = letter
+    ? shapes[Math.floor(Math.random() * shapes.length)]
+    : null;
 
   const getRandomPercentage = () => {
     return `${Math.random() * 20 + 40}%`;
@@ -197,17 +145,17 @@ function App() {
   i++;
 
   return (
-    <div className='letter-container' key={i}>
-      <div className='center shape'>
-        <svg height='60vh' width='60vw' viewBox='0 0 100 100'>
+    <div className="letter-container" key={i}>
+      <div className="center shape">
+        <svg height="60vh" width="60vw" viewBox="0 0 100 100">
           {shape}
         </svg>
       </div>
 
       <div
-        className='center letter'
+        className="center letter"
         style={{
-          position: 'absolute',
+          position: "absolute",
           top: getRandomPercentage(),
           left: getRandomPercentage(),
         }}
